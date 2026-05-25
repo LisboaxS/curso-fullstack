81ffd3e317af3b11a129eac6359a9cd2572d1fa7
LisboaxS <kaue.lisboa.souza@gmail.com>
2026-05-25 16:33:03 -0300

    adicionando post

--- node-people/app.js
+++ node-people/app.js
@@ -3,6 +3,23 @@ const { log } = require('node:console')
 const app = express()
 const port = 3000
 
+//mock
+const nomes = [
+    { id: 1, nome: "Fernanda", idade: 18, casado: true },
+    { id: 2, nome: "Juliana", idade: 22, casado: false },
+    { id: 3, nome: "Doris", idade: 15, casado: false }
+]
+
+
+app.use(express.json())
+
+// Criando funções auxiliares
+//Retornar o objeto por id
+
+function buscarNomePorId(id){
+    return nomes.filter((nome) => nome.id == id)
+}
+
 //rota principal
 app.get ('/', (req, res) => {
     res.send('Hello World!')
@@ -11,6 +28,26 @@ app.get ('/', (req, res) => {
 app.get ('/teste', (req, res) => {
     res.send('Rota do senai teste')
 })
+
+// Buscando nomes (listaNomes)
+
+app.get('/listaNomes', (req, res) => {
+    res.send(nomes)
+})
+
+//Buscando por Id
+
+app.get("/listaNomes/:id", (req,res) => {
+    let index = req.params.id
+    res.json(buscarNomePorId(index))
+})
+
+//Criando um cadastro
+app.post("/listaNomes", (req,res) =>{
+    nomes.push(req.body)
+    res.status(201).send("nomes cadastrados com sucesso!")
+})
+
 
 app.listen(port, () => {
     console.log(`Servidor rodando no endereço http://localhost:${port}`)
